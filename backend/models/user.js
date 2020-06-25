d8dbb4506ab20dd4b56053530335f18a6a26836c
Cai heng <ianyaheng822@gmail.com>
2020-06-25 22:51:04 +1000

    add the authencatation

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const crypto= require('crypto')
+// const { timeStamp } = require('console')
 
 const userScheme =new mongoose.Scheme({
   username:{
@@ -50,5 +51,48 @@ const userScheme =new mongoose.Scheme({
     default:''
   }
 },{timestamp:true})
+
+
+userScheme.virtual('password')
+    .set(function(password){
+      //create a temporary variable called _password
+      this._password = password
+      //generate salt
+      this.salt = this.makeSalt()
+      //encryptPassword
+      this.hashed_password = this,encryptPassword(password)
+    })
+    .get(function(){
+      return this._password
+    })
+
+    userScheme.methods = {
+
+    
+
+        authenticate: function(plainText){
+          return this.encryptPassword(plainText) === this.hashed_password
+        },
+
+        encryptPassword:function(){
+            if(!password){
+              return ''
+            }
+            try{
+              return crypto
+                    .createHmac('sha1',this.salt)
+                    .update(password)
+                    .digest('hex')
+            }catch(err){
+                return ''
+            }
+        },
+
+        makeSalt : function(){
+          return Math.round(new Date().valueOf()*Math.random())+ ''
+        }
+    }
+
+   
 
 module.exports = mongoose.model('User', userScheme)
