483193b9c3ba2abc18e868be3dcedc438f3df6d5
Cai heng <ianyaheng822@gmail.com>
2020-06-26 15:33:37 +1000

    add newUser route

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,11 +1,33 @@
 
-
+const User= require('../models/user')
+const shortid = require('shortid')
 
 
 exports.signup=(req,res)=>{
-  const {name,email,password} = req.body
-  res.json({
-    user:{name,email,password}
-  })
+    User.findOne({email:req.body.email}).exec((err, user)=>{
+      if(user){
+        return res.status(400).json({
+          error: ' Email is token'
+        })
+      }
+
+      const {name, email,password}= req.body
+      let username = shortid.generate()
+      let profile = `${process.env.CLIENT_URL}/profile/${username}`
+
+      let newUser = new User({name, email, password, profile, username})
+
+      newUser.save((err,success)=>{
+        if(err){
+          return res.status(400).json({
+            error:err
+          })
+        }
+        res.json({
+          message : 'SignUp success! Please Signin'
+        })
+      })
+
+    })
 
 }
