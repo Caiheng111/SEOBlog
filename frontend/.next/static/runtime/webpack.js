cf6158698f5d76d730a510f27f41e570dafefc9d
Cai heng <ianyaheng822@gmail.com>
2020-06-28 16:28:23 +1000

    set front end

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5b0efd4bfdb109951e97";
+/******/ 	var hotCurrentHash = "07caf5b80556e0c47061";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
